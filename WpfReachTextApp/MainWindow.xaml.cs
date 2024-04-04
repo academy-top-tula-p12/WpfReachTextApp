fd6055b034ea99182ebff0521e5c9e29fcb7726c
maxefimov <efimov.top.academy@gmail.com>
2024-04-04 21:09:40 +0300

    Добавьте файлы проекта.

---
+++ WpfReachTextApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfReachTextApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "RTF files(*.rtf)|*.rtf|All files (*.*)|*.*";
+
+            if(openFile.ShowDialog() == true)
+            {
+                TextRange textDocument 
+                    = new TextRange(
+                    docBox.Document.ContentStart, 
+                    docBox.Document.ContentEnd);
+                using(FileStream stream = new(openFile.FileName, FileMode.Open))
+                {
+                    if (System.IO.Path.GetExtension(openFile.FileName).ToLower() == ".rtf")
+                        textDocument.Load(stream, DataFormats.Rtf);
+                    else if (System.IO.Path.GetExtension(openFile.FileName).ToLower() == ".txt")
+                        textDocument.Load(stream, DataFormats.Text);
+                    else
+                        textDocument.Load(stream, DataFormats.Xaml);
+                }
+            }
+        }
+    }
+}
